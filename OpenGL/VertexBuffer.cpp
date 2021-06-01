1302d77fda249f9e5871a18c6e697f8e25ef1d32
OzlFX <39440462+OzlFX@users.noreply.github.com>
2021-06-01 21:14:37 +0100

    More debugging

--- OpenGL/VertexBuffer.cpp
+++ OpenGL/VertexBuffer.cpp
@@ -23,12 +23,12 @@ void VertexBuffer::UnBind() const
 	glBindBuffer(GL_ARRAY_BUFFER, 0); //Unbind
 }
 
-void VertexBuffer::SetData(const GLfloat* _Data, int _ComponentSize)
+void VertexBuffer::SetData(const void* _Data, int _ComponentSize)
 {
 	m_DataSize = sizeof(_Data);
 	m_Components = _ComponentSize;
 	//Upload a copy of the data from memory to VBO
-	glBufferData(GL_ARRAY_BUFFER, m_DataSize * sizeof(GLfloat), _Data, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, m_Components * sizeof(GLfloat), _Data, GL_STATIC_DRAW);
 }
 
 VertexBuffer::~VertexBuffer()
